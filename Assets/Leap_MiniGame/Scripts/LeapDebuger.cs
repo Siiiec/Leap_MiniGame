66f3e0a58bdccb2b5a5133d9e1a2a22f12f5464e
Siiiec <20ca39mg@gmail.com>
2018-05-30 15:06:05 +0900

    ShaderGraph

---
+++ Assets/Leap_MiniGame/Scripts/LeapDebuger.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity;
+using System.Linq;
+using UnityEngine.UI;
+
+public class LeapDebuger : MonoBehaviour
+{
+    public enum LR
+    {
+        L, R
+    }
+
+    public Text text;
+    public LR lr = LR.L;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        var hand = GetHand(lr);
+
+        if (hand != null)
+        {
+            //text.text = $"FistStrength: {hand?.GetFistStrength()}\n" +
+            //    $"PinchPos: {hand.GetPinchPosition()}\n" +
+            //    $"PredictedPinchPos: {hand.GetPredictedPinchPosition()}\n" +
+            //    $"GrabAngle: { hand.GrabAngle}\n" +
+            //    $"GrabStrength: { hand.GrabStrength}\n" +
+            //    $"IsPinching: { hand.IsPinching()}\n" +
+            //    $"PalmarAxis: { hand.PalmarAxis()}\n" +
+            //    $"PalmNormal: { hand.PalmNormal}\n" +
+            //    $"PalmPos: { hand.PalmPosition}\n" +
+            //    $"PalmVelo: { hand.PalmVelocity}\n" +
+            //    $"PalmWidth: { hand.PalmWidth}\n" +
+            //    $"PinchDistance: { hand.PinchDistance}\n" +
+            //    $"PinchStreangth: { hand.PinchStrength}\n" +
+            //    $"RadialAxis: { hand.RadialAxis()}\n" +
+            //    $"Rotation: { hand.Rotation.ToQuaternion().eulerAngles}\n" +
+            //    $"StabilizedPalmPos: { hand.StabilizedPalmPosition}\n" +
+            //    $"TimeVisible: { hand.TimeVisible}\n"
+            //;
+        }
+        else
+        {
+            text.text = "Hand is not detected";
+        }
+        
+    }
+
+    Leap.Hand GetHand(LR lr)
+    {
+        var hands = LeapHandAccessor.hands;
+        return hands.FirstOrDefault(h => lr == LR.L ? h.IsLeft : h.IsRight);
+    }
+}
